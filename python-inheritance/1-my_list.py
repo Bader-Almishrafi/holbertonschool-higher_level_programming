52dd930283b0e04caac0d12a3b17c2a87a5575f8
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-01-30 10:25:47 +0300

    Refactor MyList class error handling and improve print_sorted method documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,22 +9,28 @@ class MyList(list):
         """Initialize the list (doctest-compatible error message)."""
         if len(args) > 1:
             raise TypeError(
-                "list() takes at most 1 argument ({} given)".format(len(args)))
+                "list() takes at most 1 argument ({} given)".format(len(args))
+            )
         super().__init__(args[0] if args else [])
 
     def print_sorted(self):
-        """Prints a sorted version of the list (ascending) without modifying it."""
+        """Prints a sorted version of the list (ascending)."""
         try:
             print(sorted(self))
         except TypeError:
-            # Doctest expects this exact message when mixing str and int
-            has_str = False
-            has_int = False
+            first = None
+            second = None
+
             for x in self:
-                if isinstance(x, str):
-                    has_str = True
-                elif isinstance(x, int):
-                    has_int = True
-            if has_str and has_int:
-                raise TypeError("unorderable types: str() < int()")
+                t = type(x).__name__
+                if first is None:
+                    first = t
+                elif t != first:
+                    second = t
+                    break
+
+            if first and second:
+                raise TypeError(
+                    "unorderable types: {}() < {}()".format(first, second)
+                )
             raise
