4778aeadc07d326e2c198fcbb130223ecf95e2ec
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-01-30 10:36:40 +0300

    Update docstring in 3-is_kind_of_class.py for clarity

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""3-is_kind_of_class.py Check if an object is an instance of a class or inherited from it."""
+"""Check if an object is an instance of a class or inherited from it."""
 
 
 def is_kind_of_class(obj, a_class):
