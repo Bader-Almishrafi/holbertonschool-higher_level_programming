92276d27f4ecdffdf0da95d994e17968b2448c61
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-02-02 19:31:33 +0300

    Fix indentation and formatting in from_json_string function documentation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """4-from_json_string.py Defines a JSON-to-object function."""
 
+
 def from_json_string(my_str):
-	"""Converts a JSON string to a Python object.
+    """Converts a JSON string to a Python object.
 
-	Args:
-					my_str (str): The JSON string to convert.
+    Args:
+                                    my_str (str): The JSON string to convert.
 
-	Returns:
-					any: The Python object representation of the JSON string.
-	"""
-	import json
+    Returns:
+                                    any: The Python object representation of the JSON string.
+    """
+    import json
 
-	return json.loads(my_str)
+    return json.loads(my_str)
