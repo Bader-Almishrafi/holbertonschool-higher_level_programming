2037bc770b36365313254c7631973c4ebbb367e8
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-02-02 19:39:41 +0300

    update doc

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""5-save_to_json_file.py Defines a function to save a object to a file in JSON"""
+"""5-save_to_json_file.py Defines save object to file in JSON"""
 
 
 def save_to_json_file(my_obj, filename):
     """Saves a Python object to a file in JSON format.
 
     Args:
-                                    my_obj (any): The Python object to save.
-                                    filename (str): The name of the file to save to.
+        my_obj (any): The Python object to save.
+        filename (str): The name of the file to save to.
     """
     import json
 
