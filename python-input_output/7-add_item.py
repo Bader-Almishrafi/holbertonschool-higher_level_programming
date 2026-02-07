5be94abe2e087005d035c01396f47941cbff3a59
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-02-07 13:08:36 +0300

    Fix: add missing docstring to 7-add_item script

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+7-add_item.py adds all command-line arguments to a Python list
+and saves them to a JSON file.
+"""
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
