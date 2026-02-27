2fafc934382265f233d5c8d86c2bf379b3d83c3a
Bader-Almishrafi <baderalmishrafi@gmail.com~>
2026-02-27 20:18:12 +0300

    Refactor database connection and query to list states starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python3
-import sys
+"""Lists all states with a name starting with N"""
+
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    #just Ex
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    )
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
