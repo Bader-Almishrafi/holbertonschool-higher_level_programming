aceaaeaa8dcf9c43ff591a8e80debfa2c17da690
Bader-Almishrafi <baderalmishrafi@gmail.com~>
2026-02-27 20:32:59 +0300

    pycodestyle validation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
