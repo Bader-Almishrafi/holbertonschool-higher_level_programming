c9861d09697266d220f009bb81ea2154ba4abbc1
Bader-Almishrafi <baderalmishrafi@gmail.com~>
2026-02-27 17:20:10 +0300

    edit 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,5 @@
 -- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");-- Inserts or updates a row in first_table
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School')
+ON DUPLICATE KEY UPDATE name = 'Best School';
