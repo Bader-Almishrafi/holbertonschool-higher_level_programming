91e17bc01a2293953178c953fe25fa652f5dbe5d
Bader-Almishrafi <baderalmishrafi@gmail.com~>
2026-02-27 22:43:05 +0300

    Refactor SQL query in filter_states.py to use BINARY for case-sensitive matching

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,12 +20,12 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY 'N%' "
+        "ORDER BY id ASC"
     )
 
-    rows = cur.fetchall()
-
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
